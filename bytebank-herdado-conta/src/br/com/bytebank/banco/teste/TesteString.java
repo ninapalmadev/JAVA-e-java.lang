16c54c4f8f180108bb25489bbe4507d4b19deae7
Nina Palma <ninapalmadev@gmail.com>
2022-09-16 18:22:48 -0300

    explorando métodos da classe String

--- bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -4,16 +4,40 @@ public class TesteString {
 
 	public static void main(String[] args) {
 		
+		String vazio = " ";
+		System.out.println(vazio.isEmpty());
+		
+		String outroVazio = vazio.trim();
+		System.out.println(outroVazio.isEmpty());
+		
 		int a = 4;
 		String nome = "pidonna"; //object literal
 		//String outro = new String("ALura");
 		System.out.println(nome);
+		System.out.println(nome.contains("nn"));
 		
-		String outra = nome.replace("a", "A");
+		String outra = nome.replace('a', 'A');
 		System.out.println(outra);
+		
+		char nova = nome.charAt(2);
+		System.out.println(nova);
+		
+		int pos = nome.indexOf("nn");
+		System.out.println(pos);
 		
 		String maiuscula = nome.toUpperCase();
 		System.out.println(maiuscula);
+		
+		String sub = nome.substring(1);
+		System.out.println(sub);
+		
+		System.out.println(nome.length());
+		
+		
+		for(int i = 0; i < nome.length(); i++) {
+			System.out.println(nome.charAt(i));
+		}
+		
 		
 	}
 
